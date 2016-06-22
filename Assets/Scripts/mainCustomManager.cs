085a1505861d1e1e63ae1e1713992f9bc378e398
patlenew <pat.lenouveau@gmail.com>
2016-06-21 23:01:50 -0400

    Add files via upload

---
+++ Assets/Scripts/mainCustomManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainCustomManager : MonoBehaviour {
+
+    public CharacterCustomUi mainCharacter;
+    public CharacterCustomUi.playerSelectionUI playerSelection;
+    public mainCharacterManager managerCharacter;
+    public GameObject[] prefabCustom;
+
+    void Awake()
+    {
+        mainCharacter = new CharacterCustomUi();
+      //  managerCharacter = new mainCharacterManager();
+
+        prefabCustom  = Resources.LoadAll<GameObject>("Menu");
+
+        robotManager robot = new robotManager();
+        if (playerSelection.playerObject != null)
+            Destroy(playerSelection.playerObject);
+        // managerCharacter.selectPlayerList[0].setPlayerIndex(0);
+        onAssignRobot(/*managerCharacter.selectPlayerList[0],*/ 1);
+        Debug.Log(playerSelection.SelectionIndex);
+    }
+
+
+
+       void onAssignRobot(/*CharacterCustomUi.playerSelectionUI pUI*/ int previousIndex)
+       {
+           if (playerSelection.playerObject != null)
+               Destroy(playerSelection.playerObject);
+
+           Player robot = robotManager.Database[playerSelection.SelectionIndex];
+           robotManager.onSetSelectRobot(playerSelection.SelectionIndex, playerSelection.PlayerIndex);
+
+           playerSelection.playerObject = Instantiate(prefabCustom[playerSelection.SelectionIndex - 1], playerSelection.customPosition.position, Quaternion.identity) as GameObject;
+           playerSelection.name.text = robot.Name;
+          onSetStatsBar(previousIndex);
+
+
+       }
+
+    public void onNextCustom(Button btn)
+    {
+        Debug.Log(playerSelection.SelectionIndex);
+     /*CharacterCustomUi.playerSelectionUI pUI = playerSelection.selectPlayerList[returnpUIbyName(btn)];*/
+        int previousIndex = playerSelection.SelectionIndex;
+        playerSelection.setSelectIndex(playerSelection.SelectionIndex + 1);
+
+        if (playerSelection.SelectionIndex > prefabCustom.Length)
+            playerSelection.setSelectIndex(1);
+
+        onAssignRobot(/*playerSelection,*/ previousIndex);
+
+    }
+
+    public void onPrevCustome(Button btn)
+    {
+        
+        //CharacterCustomUi.playerSelectionUI pUI = playerSelection.selectPlayerList[returnpUIbyName(btn)];
+        int previousIndex = playerSelection.SelectionIndex;
+        playerSelection.setSelectIndex(playerSelection.SelectionIndex - 1);
+        if (playerSelection.SelectionIndex < 1)
+            playerSelection.setSelectIndex(prefabCustom.Length);
+
+        onAssignRobot(previousIndex);
+
+
+    }
+
+
+    void onSetStatsBar(int prevIndex)
+        {
+            iTween.ValueTo(playerSelection.speedShow, iTween.Hash("from", robotManager.Database[prevIndex].Speed / CharacterCustomUi.playerSelectionUI.speedMax, "to", playerSelection.speedRatio, "time", 0.3f, "onupdate", "onScale", "easetype", "easeOutCubic"));
+            iTween.ValueTo(playerSelection.healthShow, iTween.Hash("from", robotManager.Database[prevIndex].Health / CharacterCustomUi.playerSelectionUI.healthMax, "to", playerSelection.healthRatio, "time", 0.3f, "onupdate", "onScale", "easetype", "easeOutCubic"));
+        iTween.ValueTo(playerSelection.accuracyShow, iTween.Hash("from", robotManager.Database[prevIndex].Accuracy / CharacterCustomUi.playerSelectionUI.accuracyMax, "to", playerSelection.accuracyRatio, "time", 0.3f, "onupdate", "onScale", "easetype", "easeOutCubic"));
+        iTween.ValueTo(playerSelection.regenShow, iTween.Hash("from", robotManager.Database[prevIndex].RegenRate / CharacterCustomUi.playerSelectionUI.regenMax, "to", playerSelection.regenRatio, "time", 0.3f, "onupdate", "onScale", "easetype", "easeOutCubic"));
+
+    }
+
+    public void onReturn()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+        
+}
---
+++ Assets/Scripts/playerManager.cs
@@ -0,0 +1,266 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class playerManager : MonoBehaviour
+{
+    public Player player = null;
+    protected PlayerUIManager pUIManager;
+    public PlayerUIManager PlayerUIManager { get { return pUIManager; } }
+    public GameObject Weapon;
+    public GameObject ParticleObject;
+    bool active = true, alive = true;
+    public bool isActive { get { return active; } }
+    public bool isAlive { get { return alive; } }
+
+
+
+    public void onSetAlive(bool state)
+    {
+        alive = state;
+    }
+
+    public void onSetActive(bool state)
+    {
+        active = state;
+    }
+
+
+    public void onActivate(int index)
+    {
+        player = robotManager.SelectedInGame[index];
+        player.onReset(gameObject, transform.GetChild(0).gameObject, transform.GetChild(1).gameObject, index);
+        ParticleObject.GetComponent<ParticleSystem>().Stop();
+    }
+
+
+    void Update()
+    {
+        if (isActive && player != null && player.gameObject != null)
+            player.onUpdate();
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        Debug.Log("Attack");
+        GameObject collider = col.gameObject;
+        if (collider.tag == "Enemy" && player.isAttacking)
+            collider.GetComponent<playerManager>().player.onMeleeHit(player.Weapon.Damage);
+
+    }
+
+
+
+}
+
+
+
+
+
+public abstract class Character
+{
+    protected bool firing, pausing, attacking, shaking;
+    protected int index;
+    protected float speed, health, maxHealth, regenRate, accuracy, shield, ammo, damage;
+
+    public bool isAttacking { get { return attacking; } }
+    public bool isPausing { get { return pausing; } }
+    public bool isFiring { get { return firing; } }
+
+
+    //Weapon info
+    protected Weapon weapon;
+    public Weapon Weapon { get { return weapon; } }
+
+
+    //Getters character info
+    public float Speed { get { return speed; } }
+    public float Health { get { return health; } }
+    public float RegenRate { get { return regenRate; } }
+    public float MaxHealth { get { return maxHealth; } }
+    public float Accuracy { get { return accuracy; } }
+    public float Shield { get { return shield; } }
+    public float Ammo { get { return ammo; } }
+    public float Damage { get { return damage; } }
+    public int Index { get { return index; } }
+
+
+    //gameObject info
+    protected GameObject gObject, particleObject;
+    public GameObject gameObject { get { return gObject; } }
+    public GameObject ParticleObject { get { return particleObject; } }
+    public void setGameObject(GameObject g) { gObject = g; }
+    public void setParticleObject(GameObject g) { particleObject = g; }
+    public Vector3 Position { get { return gObject.transform.position; } }
+
+    //Basic Functions
+    public abstract void onReset(GameObject playerObject, GameObject weaponObject, GameObject particleObject, int index);
+
+    protected void onRotate()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y,
+            gameObject.transform.position.z - Camera.main.transform.position.z));
+        Vector3 eA = gameObject.transform.rotation.eulerAngles;
+        gameObject.transform.localEulerAngles = new Vector3(eA.x, eA.y, Mathf.Atan2((screenPos.y - gameObject.transform.position.y), (screenPos.x - gameObject.transform.position.x)) * Mathf.Rad2Deg);
+    }
+
+    public Character()
+    {
+        maxHealth = health;
+    }
+
+    protected virtual void onMove()
+    {
+
+    }
+
+
+
+    public IEnumerator onShake()
+    {
+        if (!shaking)
+        {
+            shaking = true;
+            firing = true;
+            UIEffects.onEnableShake(0.1f);
+            Vector2 shake = Random.insideUnitCircle;
+            gameObject.transform.position += (new Vector3(shake.x, shake.y, 0) * weapon.FireType.ShakeRate);
+            firing = false;
+            shaking = false;
+        }
+
+        yield break;
+    }
+
+    protected virtual void onAttack() { }
+
+    protected void onDeath()
+    {
+
+        MonoBehaviour.Destroy(gameObject);
+        combatManager.onPlayerDeath(index);
+
+
+    }
+    public void onWeaponHit(float damage)
+    {
+
+        onHit(damage);
+
+    }
+    public void onMeleeHit(float damage)
+    {
+        onHit(damage);
+    }
+
+    public void onHit(float damage)
+    {
+        pausing = true;
+        health -= damage;
+        UIManager.onHealthChange(index);
+        if (health < 0)
+        {
+            onDeath();
+            return;
+        }
+
+    }
+
+    public IEnumerator onPause()
+    {
+        if (!pausing)
+        {
+            pausing = true;
+            yield return new WaitForEndOfFrame();
+            pausing = false;
+            yield break;
+        }
+
+    }
+
+    protected void onRegenerateHealth()
+    {
+        if (health < maxHealth && health % 33 > 0)
+        {
+            health += regenRate;
+            UIManager.onHealthChange(index);
+        }
+
+    }
+
+    public abstract void onUpdate();
+
+
+
+
+}
+
+
+[System.Serializable]
+public class Player : Character
+{
+    protected IUltimate ultimate;
+    public IUltimate Ultimate { get { return ultimate; } }
+
+    protected IMovable movable;
+    public IMovable Movable { get { return movable; } }
+
+    protected string name;
+    public string Name { get { return name; } }
+
+    protected virtual void onCheckUltimate() { }
+
+
+    public Player()
+    {
+
+    }
+
+    public override void onReset(GameObject playerObject, GameObject weaponObject, GameObject ParticleObject, int _index)
+    {
+        gObject = playerObject;
+        particleObject = ParticleObject;
+        weapon = new Pistol(this, weaponObject);
+        index = _index;
+    }
+
+    public override void onUpdate()
+    {
+        if (weapon != null)
+            weapon.onFire();
+
+        movable.onMove(gameObject);
+        onAttack();
+        onRotate();
+        onRegenerateHealth();
+        ultimate.onUpdate();
+    }
+
+
+
+
+
+    protected override void onAttack()
+    {
+        attacking = Input.GetButtonDown("Attack");
+        gameObject.GetComponent<MeshRenderer>().materials[0].color = attacking ? Color.black : Color.red;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
