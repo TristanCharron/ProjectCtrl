99df5884024fc07d435ffbac7bbc46316c210d22
patlenew <pat.lenouveau@gmail.com>
2016-08-20 03:19:51 -0400

    0.4 UI Manager

--- New Unity Project/Assets/Scripts/UI/UiManager.cs
+++ New Unity Project/Assets/Scripts/UI/UiManager.cs
@@ -8,26 +8,35 @@ public class UiManager : MonoBehaviour {
     public GameObject titleContainer;
     public GameObject playerIDcontainer;
     public List<Text> playerId = new List<Text>();
-    float duration = 1, alpha = 0;
+    float duration = 1, alpha = 0, t;
     int nbPlayers;
+
 
 	// Use this for initialization
 	void Start () {
+        
         nbPlayers = 4;
         playerIDcontainer.SetActive(false);
+       
         //iTween.MoveTo(gameObject, iTween.Hash("x", 3, "time", 4, "delay", 1, "onupdate", "myUpdateFunction", "looptype", iTween.LoopType.pingPong));
 
     }
 
     // Update is called once per frame
     void Update () {
-	
-        if(Input.GetMouseButtonDown(0))
+        
+
+       
+
+        
+        if (Input.GetMouseButtonDown(0))
         {
             titleContainer.SetActive(false);
             gameObject.GetComponent<Animator>().enabled = true;
             StartCoroutine(makeIDAppear());
         }
+
+       
 	}
 
     IEnumerator makeIDAppear()
@@ -44,6 +53,15 @@ public class UiManager : MonoBehaviour {
             g.CrossFadeAlpha(0.0f, 2.0f, false);
         }
     }
+
+   /* void chargeBarActive()
+    {
+        
+        chargeBar.SetActive(true);
+        chargeBar.transform.localPosition = new Vector3( Mathf.Lerp(-0.255f, 0.2569f, t ) , chargeBar.transform.localPosition.y, chargeBar.transform.localPosition.z);
+    }
+    **/
+
 
     float lerp()
     {
---
+++ New Unity Project/Assets/Scripts/UI/monkChargeBall.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monkChargeBall : MonoBehaviour {
+
+    float t;
+    public GameObject chargeBar, meterReload;
+    bool isChargeActive;
+
+	// Use this for initialization
+	void Start () {
+        chargeBar.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (t < 1 && isChargeActive)
+        {
+            t += Time.deltaTime;
+
+            chargeBar.SetActive(true);
+            chargeBar.transform.localPosition = new Vector3(Mathf.Lerp(-0.255f, 0.2569f, t), chargeBar.transform.localPosition.y, chargeBar.transform.localPosition.z);
+
+        }
+        else
+        {
+            meterReload.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(chargeBar.transform.localPosition.x < -0.1006f || chargeBar.transform.localPosition.x > -0.1465f)
+            {
+                Debug.Log("patrice keka");
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            callCharge();
+        }
+       
+    }
+
+    public void callCharge()
+    {
+        t = 0;
+        isChargeActive = true;
+        meterReload.SetActive(true);
+    }
+}
