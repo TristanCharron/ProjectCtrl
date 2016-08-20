12a4014176ea54836556f4d3a299cb441822e9e8
Tristan Charron <charron.tristan@gmail.com>
2016-08-19 23:54:50 -0400

    First physics with Ball

--- New Unity Project/Assets/Scripts/Ball/ballManager.cs
+++ New Unity Project/Assets/Scripts/Ball/ballManager.cs
@@ -1,27 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ballManager : MonoBehaviour {
+public class ballManager : MonoBehaviour
+{
 
     public static ballManager Instance { get { return instance; } }
     static ballManager instance;
 
-    private static float currentVelocity,minVelocity,maxVelocity; //Speed variables
+    private static Vector3 velocityAngle; // angle pushing the ball
 
-    public static float MaxVelocity { get { return maxVelocity; } }
+    public static Vector3 VelocityAngle { get { return velocityAngle; } }
 
-    public static float MinVelocity { get { return minVelocity; } }
+    private static float currentVelocity; //Speed variables
 
-    public static float CurrentSpeed { get { return Mathf.Clamp(currentVelocity, minVelocity, maxVelocity); } } //Clamp & Return speed
+    public static float MaxVelocity { get { return Instance._MaxVelocity; } }
 
-    public float _MaxVelocity, _MinVelocity;
+    public static float MinVelocity { get { return Instance._MinVelocity; } }
+
+    public static float CurrentVelocity { get { return Mathf.Clamp(currentVelocity, MinVelocity, MaxVelocity); } } //Clamp & Return speed
+
+    public static float DecreaseVelocity { get { return Instance._DecreaseVelocity; } }
+
+    public float _MaxVelocity, _MinVelocity, _DecreaseVelocity;
 
     private Rigidbody rBody;
-    
+
+    private static bool isPushed;
 
 
-	// Use this for initialization
-	void Awake () {
+
+    // Use this for initialization
+    void Awake()
+    {
         onSetComponents();
         onSetProperties();
 
@@ -35,26 +45,56 @@ public class ballManager : MonoBehaviour {
 
     private void onSetProperties()
     {
-        minVelocity = _MinVelocity;
-        maxVelocity = _MaxVelocity;
-        currentVelocity = minVelocity;
+        currentVelocity = MinVelocity;
+        velocityAngle = Vector3.forward;
+        isPushed = false;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        onMove();
 
+    public static void onPush(Vector3 newAngle, float velocityApplied)
+    {
+        velocityAngle = newAngle;
+        Instance.StartCoroutine(Instance.onLerpPushedBall(velocityApplied, 0.05f));
+    }
+
+
+    public IEnumerator onLerpPushedBall(float destVelocity, float length)
+    {
+        if (!isPushed)
+        {
+            isPushed = true;
+            float timer = 0;
+            // Lerp velocity
+            while (timer <= length)
+            {
+                currentVelocity = Mathf.Lerp(currentVelocity, destVelocity, timer);
+                timer += Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+            timer = 0;
+          
+            isPushed = false;
+        }
+        yield break;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        onMove();
     }
 
     private void onChangeVelocity()
     {
-
+        currentVelocity -= DecreaseVelocity;
+        currentVelocity = Mathf.Clamp(currentVelocity, MinVelocity, MaxVelocity);
     }
-    
+
     private void onMove()
     {
-        rBody.AddForce(Vector3.forward * currentVelocity);
-        currentVelocity--;
-
+        rBody.AddForce(transform.forward * currentVelocity);
+        onChangeVelocity();
     }
+
+
+
+
 }
