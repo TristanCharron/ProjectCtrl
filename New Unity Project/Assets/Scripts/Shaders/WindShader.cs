e3428c8d9d16fb26b1b0742dacfb4dfc603d65d5
Tristan Charron <charron.tristan@gmail.com>
2016-09-18 14:39:39 -0400

    Shader fixing

--- New Unity Project/Assets/Scripts/Shaders/WindShader.cs
+++ New Unity Project/Assets/Scripts/Shaders/WindShader.cs
@@ -8,10 +8,11 @@ public class WindShader : ImageEffectBase
 {
     private static WindShader instance;
     public static WindShader Instance { get { return instance; } }
+
     public const float length = 3;
 
     //Shader parameters transfered to Shader
-    [Range(0.1f, 100f)]
+    [Range(0.1f, 20f)]
     public float intensity = 100f;
     private float disabledIntensity = 0.5f, currentIntensity;
     static bool isEnabled = false;
